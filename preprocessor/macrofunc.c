019568ee7b9262500aa98a18dae2c47aa2b55b09
arjun-exe07 <arjunkadam202007@gmail.com>
2026-06-06 12:24:16 +0530

    Modified file

--- preprocessor/macrofunc.c
+++ preprocessor/macrofunc.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <ctype.h>
 #define PI 3.14159265359
 #define area(r) (PI * r * r)
 
 void main()
 {
   // 3.141593
-  printf("%f", area(10));
+  printf("%f\n", area(10));
+
+  printf("%d", isdigit(12));
 }
