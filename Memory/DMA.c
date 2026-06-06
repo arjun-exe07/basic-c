05fe46ab6997a1e4773ed4a179942ddc8b0efa77
arjun-exe07 <arjunkadam202007@gmail.com>
2026-06-06 12:47:42 +0530

    Added dynamic memory allocationn

---
+++ Memory/DMA.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  int n;
+  printf("Enter the size of array :");
+  scanf("%d", &n);
+
+  int *arr = (int *)malloc(n * sizeof(int));
+  if (arr == NULL)
+  {
+    printf("Memory allocation failed.");
+    return 0;
+  }
+
+  free(arr);
+
+  return 0;
+}
